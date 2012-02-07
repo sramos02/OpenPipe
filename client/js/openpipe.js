270a21caefd24fe640faf9ff8c1644839199e150
Sean Kenny <skenny214@gmail.com>
2012-02-07 00:36:27 -0500

    commenting!

--- client/js/openpipe.js
+++ client/js/openpipe.js
@@ -19,29 +19,32 @@
 
 	 var op = {};
 
-
+	 //init the OpenPipe client - hide pipelets initally (no FLOCs)
 	 op.init = function(){
 		$('html').append('<div id="op-loading">loading</div>');
 		$('.pipelet').hide();
 	 };
 
-
+	 //load a given segment object into the piplined document
 	 op.load = function(segment){
 		this.loadSegment(segment);
 	 };
 
 
+	 //register a given phase from a segment
 	 op.registerPhase = function(phase){
 		if(phase == undefined) phase = 0;
 		if(phases[phase] == undefined) phases[phase] = phase;
 	 };
 
+	 //mark a phase complete. A completed phase has its deferred JavaScript elemetns loaded (and consequently run)
 	 op.phaseComplete = function(phase){
 		lastPhase = phase;
 		this.loadScripts(phase);
 	 };
 	
 	
+	 //mark the pipeline as completed. For all the phases mark complete if not already done (load all JavaScript for any phases where complete notification was not sent)
 	 op.done = function(){
 		var that = this;
 		_.each(phases, function(phase){
@@ -52,7 +55,7 @@
 
 
 
-
+	//load an pipeline segment. A segment contains an id (element to load on page), css, html and JavaScript. The id is found, css is loaded, html is loaded , and JavaScript is queued until the end of the segments phase
 	 op.loadSegment = function(segment){
 		if(isFirstSegment === true){
 			isFirstSegment = false;
@@ -70,13 +73,14 @@
 
 
 
-
+	//load html into the given #id'ed element by appending
 	 op.loadHtml = function(html, id){
 		if(id == undefined) id = 'content';
 		
 		$('#'+id).append(html);
 	 };
 
+	//load css into the head of the documents
 	 op.loadCss = function(css){
 		
 		var that = this;
@@ -89,7 +93,7 @@
 
 
 
-
+	//push a set of script blocks onto a phase of the pipeline cycle. Once the phase is marked complete all the scripts in that phase will be loaded (and executed)
 	 op.pushScripts = function(scripts, phase){
 		
 		var that = this;
@@ -99,6 +103,7 @@
 
 	 };
 
+	//push a single script block onto a phase of the pipeline cycel. Once the phase is marked complete all the script in that pahse will be loaded (and execueted)
 	 op.pushScript = function(script, phase){
 		if(phase == undefined) phase = 0;
 		if(scripts[phase] == undefined) scripts[phase] = [];

afbca8738073064e899ae859c893ec6f957ff67e
Sean Kenny <skenny@napc.com>
2012-04-29 01:38:08 -0400

    ...

---
+++ example/frameset.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>OpenPipe - SideBySide</title>
+</head>
+<frameset cols="50%,50%">
+<frame src="http://localhost/dev/openpipe/example/codeigniter/2.1.0/index.php/fakebook" />
+<frame src="http://localhost/dev/openpipe/example/codeigniter/2.1.0/index.php/fakebook?nopipe" />
+</frameset>
+</html> 
